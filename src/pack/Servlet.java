e1ac58357dbab0be3ed5e58c6b39ef964e5b34a8
Manuel Alejandro Developer <maco0006@red.ujaen.es>
2018-02-07 19:30:50 +0100

    Elimino archivos de prueba

--- src/pack/Servlet.java
+++ src/pack/Servlet.java
@@ -1,7 +1,7 @@
 package pack;
 
-import java.io.PrintWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Enumeration;
 
 import javax.servlet.ServletException;
@@ -11,18 +11,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class RequestServlet
+ * Servlet implementation class Servlet
  */
-@WebServlet("/RequestServlet")
-public class RequestServlet extends HttpServlet {
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-       
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.setContentType("text/plain");
 	    PrintWriter out = response.getWriter();
 
-	    out.println("Request Headers:");
+	    out.println("Cabeceras de petición:");
 	    Enumeration<String> names = request.getHeaderNames();
 	    while (names.hasMoreElements()) {
 	      String name = (String) names.nextElement();
