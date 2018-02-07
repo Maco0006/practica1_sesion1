e8cf1a4ed6ab8234c75d44a08f6b22402cbfa99d
Manuel Alejandro Developer <maco0006@red.ujaen.es>
2018-02-07 22:24:00 +0100

    Clase HttpServletRequest
    Implementado metodo getHeaderNames y getHeader
    
    Errores:
    
    No me coge las etiquetas HTML!

--- src/pack/Servlet.java
+++ src/pack/Servlet.java
@@ -10,9 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Servlet
- */
+
 @WebServlet("/Servlet")
 public class Servlet extends HttpServlet {
 	
@@ -21,23 +19,43 @@ public class Servlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/plain");
 	    PrintWriter out = response.getWriter();
-
-	    out.println("Cabeceras de petición:");
 	    
-	    /*Objeto names guarda la lista de cabeceras*/
+	    String title = "HTTP Header Request Example";
+	    String docType = "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n";
+	    out.println(docType + "<html>\n" + "<head><title>" + title + "</title></head>\n"
+	    		+ "<body>\n<h1 aling=\"center\">" + title + "</h1>\n"
+	    		+ "<table width=\"100%\" border=\"1\" align=\"center\">\n"
+	    		+ "<tr>\n"
+	    		+ "<th>Header Name</th><th>Header Value</th>\n" + "</tr>\n");
+	    //out.println("Cabeceras de petición");
+	    // Metodo getHeaderNames
+	    // Objeto names guarda la lista de cabeceras
 	    Enumeration<String> names = request.getHeaderNames();
 	    while (names.hasMoreElements()) {
-	    	/*Variable name guarda el nombre de cabecera*/
+	    	// Variable name guarda el nombre de cabecera
 	    	String name = (String) names.nextElement();
-	    	/*Objeto values guarda la lista de valores de las cabeceras*/
+	    	// Objeto values guarda la lista de valores de las cabeceras
 	    	Enumeration<String> values = request.getHeaders(name); // Soporta múltiples valores
 	    	if (values != null) {
 	    		while (values.hasMoreElements()) {
 	    			/*Variable value contiene el valor de cada cabecera*/
 	    			String value = (String) values.nextElement();
-	    			out.println(name + ": " + value);
+	    			out.println("<tr><td>" + name + "</td>\n<td>" + value + "</td></tr>\n");
 	    			}
 	    		}
 	    	}
+	    out.println("</table>\n</body></html>");
+	    /* IMPORTANTE!!! NO ME COGE LAS ETIQUETAS HTML, PERO ESTÁN BIEN ESCRITAS -.-"!
+	     * PARA COMPROBAR: EJECUTA EL SERVLET COPIA EL CODIGO QUE SALE, PEGALO EN UN
+	     * DOCUMENTO HTML Y EJECUTALO EN EL NAVEGADOR, SALDRÁ EL RESULTADO COMO ESPERAMOS.
+	     */
+	    
+	    // Metodo getHeader
+	    // Seleccionamos una cabecera y guardamos su valor en la variable
+	    String acceptLanguage = request.getHeader("accept-language");
+	    String acceptCharset = request.getHeader("accept-charset");
+	    // Mostramos el valor de la cabecera seleccionada
+	    out.println("\nacceptLanguage: " + acceptLanguage);
+	    out.println("acceptCharset:" + acceptCharset);
 	    }
 	}
