27438f078229feeb0dfc03cdf73973437c15b2f8
Marta <Marta@DESKTOP-M314RGH>
2018-02-11 11:36:13 +0100

    Solución del problema de la librería JRE y método getAuthType

--- src/pack/Servlet.java
+++ src/pack/Servlet.java
@@ -11,6 +11,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
+//Para solucionar el problema de la libreria JRE (al importar el proyecto desde github, no aparece):
+//Project --> Properties --> Java Build Path.
+//Selecciono la entrada JRE con error.
+//Clic en el boton Edit.
+//Selecciono un JRE alternativo.
+//Boton a Finish.
+
+//Ademas, despues me daba un error de servidor, por lo que aplique el siguiente cambio:
+//Project --> Properties --> Java Build Path --> Libraries.
+//Clic en Add External Jars.
+//Selecciono la dirección: /Tomcat/Bin/tomcat-juli.jar.
+//Aplicar cambios.
+
+
+
 @WebServlet("/Servlet")
 public class Servlet extends HttpServlet {
 	
@@ -56,6 +71,21 @@ public class Servlet extends HttpServlet {
 	    // Mostramos el valor de la cabecera seleccionada
 	    out.println("<tr><td>Accept-Language</td>\n<td>" + acceptLanguage + "</td></tr>\n");
 	    out.println("<tr><td>Accept-Charset</td>\n<td>" + acceptCharset + "</td></tr>\n");
+	    
+	    out.println("</table>\n");
+	    out.println("<h1 align=\"center\"> Method getAuthType </h1>\n"
+	    		+ "<table width=\"100%\" border=\"1\" align=\"center\">\n"
+	    		+ "<tr>\n"
+	    		+ "<th>Name</th><th>Value</th>\n" + "</tr>\n");
+	    
+	    // Metodo getAuthType
+	    out.println("<tr><td>User name</td>\n<td>" + request.getRemoteUser() + "</td></tr>\n");
+	    String name = (request.getUserPrincipal() == null) ? null : request.getUserPrincipal().getName();
+	    out.println("<tr><td>Principal name</td>\n<td>" + name + "</td></tr>\n");
+	    out.println("<tr><td>Authentication Type</td>\n<td>" + request.getAuthType() + "</td></tr>\n");
+	    out.println("<tr><td>Is a Manager</td>\n<td>" + request.isUserInRole("manager") + "</td></tr>\n");
+	    
+	    
 	    out.println("</table>\n</body>\n</html>");
 	    }
 	}
